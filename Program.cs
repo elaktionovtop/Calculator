033c4b801c46ddfe63daf302a5565f10e69656cb
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-01-17 11:39:00 +0300

    Сложение двух чисел

--- Program.cs
+++ Program.cs
@@ -9,6 +9,17 @@
 
 WriteTitle("Калькулятор");
 
+Console.Write("Введите первое число: ");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+
+Console.Write("Введите второе число: ");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+
+int result = number1 + number2;
+Console.WriteLine($"Результат: {result}");
+
 ExitApp();
 
 void WriteTitle(string title)
