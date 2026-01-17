8b9db9f8c177fcc0860b568a7dc909fbdad05895
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-01-17 11:26:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// ВывестиЗаголовок
+// запросить число
+// считать текст
+// преобразовать текст в число
+// сделать то же самое со вторым числом
+// сложить два числа
+// вывести результат
+// Выйти
+
+WriteTitle("Калькулятор");
+
+ExitApp();
+
+void WriteTitle(string title)
+{
+    Console.WriteLine(title);
+    Console.WriteLine(new string('-', title.Length));
+    Console.WriteLine();
+}
+
+void ExitApp()
+{
+    Console.WriteLine();
+    Console.Write("Для выхода нажми Enter");
+    Console.ReadLine();
+}
+
+
