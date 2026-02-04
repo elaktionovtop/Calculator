3ec7f138ccd4cdb7a0e71f51e77ae970d65810eb
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-04 14:03:48 +0300

    Вычисления с повторением

--- Program.cs
+++ Program.cs
@@ -1,28 +1,29 @@
 ﻿// ВывестиЗаголовок
-// запросить число
-// считать текст
-// преобразовать текст в число
-// получить знак операции  (+ - * /)
-// ввести второе число
-// выполнить операцию в зависимости от знака
-// вывести результат
+// цикл
+//  считать первое число
+//  если отказ 
+//      выйти
+//  получить знак операции  (+ - * /)
+//  если отказ 
+//      выйти
+//  считать второе число
+//  если отказ 
+//      выйти
+//  выполнить операцию в зависимости от знака
+//  вывести результат
 // Выйти
 
 WriteTitle("Калькулятор");
+while(true)
+{
+    if(!TryReadDouble("Введите первое число: ", out double number1)) break;
+    string operation = GetOperationSign();
+    if (string.IsNullOrEmpty(operation)) break;
+    if(!TryReadDouble("Введите второе число: ", out double number2)) break;
 
-Console.Write("Введите первое число: ");
-string input = Console.ReadLine();
-double number1 = double.Parse(input);
-
-string operation = GetOperationSign();
-
-Console.Write("Введите второе число: ");
-input = Console.ReadLine();
-double number2 = double.Parse(input);
-
-double result = Calculate(number1, number2, operation);
-Console.WriteLine($"{number1} {operation} {number2} = {result:F2}");
-
+    Console.WriteLine($"{number1} {operation} {number2} = {Calculate(number1, number2, operation):F2}");
+    Console.WriteLine();
+}
 ExitApp();
 
 // ======================== Методы ========================
@@ -34,11 +35,31 @@ void WriteTitle(string title)
     Console.WriteLine();
 }
 
+bool  TryReadDouble(string prompt, out double number)
+{
+    Console.Write(prompt);
+    string input = Console.ReadLine();
+    if (string.IsNullOrEmpty(input))
+    {
+        number = 0;
+        return false;
+    }
+    bool success = double.TryParse(input, out number);
+    while (!success)
+    {
+        Console.Write("Ошибка! Введите корректное число: ");
+        input = Console.ReadLine();
+        success = double.TryParse(input, out number);
+    }
+    return success;
+}
+
 string  GetOperationSign()
 { 
     Console.Write("Введите знак операции (+ - * /): ");          
     string operation = Console.ReadLine();
-    while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+    while (operation != "+" && operation != "-" && operation != "*" && operation != "/"
+        && operation != "")
     {
         Console.Write("Ошибка! Введите корректный знак операции (+ - * /): ");
         operation = Console.ReadLine();
