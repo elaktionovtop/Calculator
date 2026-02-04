1b8899bf156fc24e12df57f62c0e311f79af8b77
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2026-02-04 13:36:31 +0300

    Обработка ошибки в знаке Метод - Вычисление

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // запросить число
 // считать текст
 // преобразовать текст в число
-// запросить знак операции  (+ - * /)
+// получить знак операции  (+ - * /)
 // ввести второе число
 // выполнить операцию в зависимости от знака
 // вывести результат
@@ -14,41 +14,60 @@ Console.Write("Введите первое число: ");
 string input = Console.ReadLine();
 double number1 = double.Parse(input);
 
-Console.Write("Введите знак операции (+ - * /): ");          
-string operation = Console.ReadLine();
+string operation = GetOperationSign();
 
 Console.Write("Введите второе число: ");
 input = Console.ReadLine();
 double number2 = double.Parse(input);
 
-double result = 0;
-
-if (operation == "+")
-{
-    result = number1 + number2;
-}
-if (operation == "-")
-{
-    result = number1 - number2;
-}
-if (operation == "*")
-{
-    result = number1 * number2;
-}
-if (operation == "/")
-{
-    result = number1 / number2;
-}
-
+double result = Calculate(number1, number2, operation);
 Console.WriteLine($"{number1} {operation} {number2} = {result:F2}");
 
 ExitApp();
+
+// ======================== Методы ========================
 
 void WriteTitle(string title)
 {
     Console.WriteLine(title);
     Console.WriteLine(new string('-', title.Length));
     Console.WriteLine();
+}
+
+string  GetOperationSign()
+{ 
+    Console.Write("Введите знак операции (+ - * /): ");          
+    string operation = Console.ReadLine();
+    while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+    {
+        Console.Write("Ошибка! Введите корректный знак операции (+ - * /): ");
+        operation = Console.ReadLine();
+    }
+    return operation;
+}
+
+double Calculate(double num1, double num2, string op)
+{
+    double res = 0;
+    switch (op)
+    {
+        case "+":
+            res = num1 + num2;
+            break;
+        case "-":
+            res = num1 - num2;
+            break;
+        case "*":
+            res = num1 * num2;
+            break;
+        case "/":
+            res = num1 / num2;
+            break;
+        default:
+            Console.WriteLine("Ошибка! Некорректный знак операции.");
+            break;
+    }
+    return res;
 }
 
 void ExitApp()
